a2ea7d1bab7fdd3612ed17d1a0e86ccd7d7e814b
dmitriy_romanchuk <roman4ukdimka@mail.ru>
2022-09-06 17:50:12 +0300

    fixes after review
    1 main.cpp is divided into tasks.
    2 Variables were initialized.
    3 Removed using namespace std

---
+++ practic_module_3/task11.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <iomanip>
+
+int main()
+{
+	// task 11:----------------------------------------------------------------------------------
+	// By default, output is right - justified in its field.You can left - justify text output using the
+	// manipulator std::setiosflags(ios::left). (For now, don’t worry about what this new notation means.) 
+	// Use this manipulator, along with std::setw()
+
+	int column_width = 18;
+
+	std::cout << std::setfill(' ') << std::setiosflags(std::ios::left) << std::setw(column_width) << "Last name" << std::setw(column_width)
+		<< "First name" << std::setw(column_width) << "Address" << std::setw(column_width) << "City" << std::endl;
+
+	std::cout << std::setfill('-') << std::setw(65) << "-" << std::endl;
+
+	std::cout << std::setfill(' ') << std::setiosflags(std::ios::left) << std::setw(column_width) << "Jones" << std::setw(column_width)
+		<< "Bernard" << std::setw(column_width) << "109 Pine Lane" << std::setw(column_width) << "Littletown" << std::endl;
+
+	std::cout << std::setiosflags(std::ios::left) << std::setw(column_width) << "O’Brian" << std::setw(column_width)
+		<< "Coleen" << std::setw(column_width) << "42 E. 99th Ave" << std::setw(column_width) << "Bigcity" << std::endl;
+
+	std::cout << std::setiosflags(std::ios::left) << std::setw(column_width) << "Wong" << std::setw(column_width)
+		<< "Harry" << std::setw(column_width) << "A Alabama St" << std::setw(column_width) << "Lakeville" << std::endl <<std::endl;
+
+
+	return 0;
+}
---
+++ practic_module_3/task9.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+int main()
+{
+	// task 9:----------------------------------------------------------------------------------
+	// If you have two fractions, a / b and c / d, their sum can be obtained from the formula
+	// a/b + c/d = (a*d + b*c)/(b*d) 
+	// For example, 1 / 4 + 2 / 3 = (1 * 3 + 4 * 2) / 4 * 3 = 11 / 12
+	// Write a program that encourages the user to enter two fractions, and then displays their
+	// sum in fractional form. (You don’t need to reduce it to lowest terms.) The interaction
+	// with the user might look like this:
+	// Enter first fraction : 1 / 2
+	// Enter second fraction : 2 / 5
+	// Sum = 9 / 10
+	// You can take advantage of the fact that the extraction operator ( >> ) can be chained to
+	// read in more than one quantity at once : std::cin >> a >> dummychar >> b;
+
+	
+	int a = 0, b = 0, c = 0, d = 0;
+	float result = 0.0f;
+	char i = ' ';
+
+	std::cout << "Enter first fraction : for example 1/2" << std::endl;
+	std::cin >> a >> i >> b;
+	std::cout << "Enter second fraction : for example 2/5" << std::endl;
+	std::cin >> c >> i >> d;
+	std::cout << "Result = " << (a * d + b * c) << i << (b * d) << std::endl << std::endl;
+
+	return 0;
+}
