f98348c319adaffbdd3b3a7de84e6de81c058370
dmitriy_romanchuk <roman4ukdimka@mail.ru>
2022-09-13 10:54:05 +0300

    Added solutions for exercises 1 - 12

---
+++ practic_module_4/task10.cpp
@@ -0,0 +1,37 @@
+﻿// Write a program that calculates how much money you’ll end up with if you invest an
+// amount of money at a fixed interest rate, compounded yearly.Have the user furnish the
+// initial amount, the number of years, and the yearly interest rate in percent.Some interaction with the program might look like this:
+// Enter initial amount : 3000
+// Enter number of years : 10
+// Enter interest rate(percent per year) : 5.5
+// At the end of 10 years, you will have 5124.43 dollars.
+// At the end of the first year you have 3000 + (3000 * 0.055), which is 3165. At the end of
+// the second year you have 3165 + (3165 * 0.055), which is 3339.08.Do this as many
+// times as there are years.A for loop makes the calculation easy
+
+#include <iostream>
+
+int main()
+{
+	float initial_amount = 0.0f;
+	float finish_amount = 0.0f;
+	float percent_per_year = 0.0f;
+	int counter_of_years = 0;
+
+	std::cout << "Enter initial amount: ";
+	std::cin >> initial_amount;
+	std::cout << "Enter finish_amount: ";
+	std::cin >> finish_amount;
+	std::cout << "Enter interest rate(percent per year): ";
+	std::cin >> percent_per_year;
+
+	while (initial_amount <= finish_amount)
+	{
+		initial_amount += (initial_amount * (percent_per_year / 100));
+		counter_of_years++;
+	}
+
+	std::cout << counter_of_years << " years";
+
+	return EXIT_SUCCESS;
+}
---
+++ practic_module_4/task11.cpp
@@ -0,0 +1,84 @@
+﻿// Create a three - function calculator for old - style English currency, where money amounts
+// are specified in pounds, shillings, and pence. (See Exercises 10 and 12 in Chapter 2.)
+// The calculator should allow the user to add or subtract two money amounts, or to multiply a money amount by a floating - point number. 
+// (It doesn’t make sense to multiply two
+// money amounts; there is no such thing as square money.We’ll ignore division.Use the
+// general style of the ordinary four - function calculator in Exercise 4 in this chapter.)
+
+#include <iostream>
+
+int main()
+{
+	char repeat = 'y';
+	char sign = ' ';
+
+	while (repeat == 'y')
+	{
+		int  pounds1 = 0, shillings1 = 0, pence1 = 0;
+		int  pounds2 = 0, shillings2 = 0, pence2 = 0;
+		int  pounds = 0, shillings = 0, pence = 0; // for results
+		float multiplay = 0.0f;
+		char ch = ' ';
+
+		std::cout << "Enter first amount: (5.10.6)"; // Запрос ввода пользователем первого значения
+		std::cin >> pounds1 >> ch >> shillings1 >> ch >> pence1;
+
+		std::cout << "Enter operation '+', '-' or '*'";// Выбор операции: сложение, вычитание, умножение и запрос на ввод значений
+		std::cin >> sign;
+		if (sign != '*')
+		{
+			std::cout << "Enter second amount: (3.2.6)";
+			std::cin >> pounds2 >> ch >> shillings2 >> ch >> pence2;
+		}
+		else
+		{
+			std::cout << "Enter multiply factor: ";
+			std::cin >> multiplay;
+		}
+		pence1 += shillings1 * 12 + pounds1 * 240; // перевод введенных сумм в пенсы
+		pence2 += shillings2 * 12 + pounds2 * 240;
+
+
+		switch (sign) // калькулятор
+		{
+		case '+':
+			pence = pence1 + pence2;
+			shillings = pence / 12;
+			while (shillings > 19)
+			{
+				shillings -= 20;
+				pounds += 1;
+			}
+			pence = pence % 12;
+			break;
+		case '-':
+			pence = pence1 - pence2;
+			shillings = pence / 12;
+			while (shillings > 19)
+			{
+				shillings -= 20;
+				pounds += 1;
+			}
+			pence = pence % 12;
+			break;
+		case '*':
+			pence = pence1 * multiplay;
+			shillings = pence / 12;
+			while (shillings > 19)
+			{
+				shillings -= 20;
+				pounds += 1;
+			}
+			pence = pence % 12;
+			break;
+			default:
+				std::cout << "Warning: \"" << ch << "\" is unknown operator" << std::endl;
+			}
+
+			std::cout << pounds << "." << shillings << "." << pence << std::endl; //Вывод результата
+			std::cout << "Do another(y / n) ? "; // воспрос повторить или нет
+			std::cin >> repeat;
+		}
+
+		return EXIT_SUCCESS;
+}
