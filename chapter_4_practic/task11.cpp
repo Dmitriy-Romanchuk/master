adc8dca492ccda2014e976bd3767414227571fe2
dmitriy_romanchuk <roman4ukdimka@mail.ru>
2022-09-21 15:47:20 +0300

    Added solution for exercises 10 - 12

---
+++ chapter_4_practic/task11.cpp
@@ -0,0 +1,51 @@
+// Use the time structure from Exercise 9, and write a program that obtains two time values 
+// from the user in 12:59:59 format, stores them in struct time variables, converts
+// each one to seconds(type int), adds these quantities, converts the result back to 
+// hoursminutes - seconds, stores the result in a time structure, and finally displays 
+// the result in 12:59:59 format.
+
+#include <iostream>
+
+int main()
+{
+	const int sec_in_hour = 3600;
+	const int sec_in_min = 60;
+	char ch = ' ';
+	long totalsec = 0;
+	
+	struct time
+	{
+		int hours;
+		int minutes;
+		int seconds;
+	};
+
+	time time1;
+	time time2;
+	time time3;
+
+	for (int i = 0; i < 2; i++)
+	{
+		std::cout << "Enter time in format hh/mm/ss: " << std::endl;
+		if(i == 0)
+			std::cin >> time1.hours >> ch >> time1.minutes >> ch >> time1.seconds;
+		else if (i == 1)
+			std::cin >> time2.hours >> ch >> time2.minutes >> ch >> time2.seconds;
+	}
+	
+	totalsec = time1.hours * sec_in_hour + time1.minutes * sec_in_min + time1.seconds + time2.hours * sec_in_hour + time2.minutes * sec_in_min + time2.seconds;
+
+	time3.hours = totalsec / sec_in_hour;
+	time3.minutes = 0;
+	time3.seconds = totalsec - time3.hours * sec_in_hour;
+
+	while (time3.seconds >= 60)
+	{
+		time3.seconds -= 60;
+		time3.minutes += 1;
+	}
+
+	std::cout << time3.hours << ":" << time3.minutes << "." << time3.seconds;
+
+	return EXIT_SUCCESS;
+}
---
+++ chapter_4_practic/task12.cpp
@@ -0,0 +1,63 @@
+﻿// Create a four - function calculator for fractions. (See Exercise 9 in Chapter 2, and
+// Exercise 4 in this chapter.) Here are the formulas for the four arithmetic operations
+// applied to fractions :
+// Addition:        a / b + c / d = (a * d + b * c) / (b * d)
+// Subtraction :    a / b - c / d = (a * d - b * c) / (b * d)
+// Multiplication : a / b * c / d = (a * c) / (b * d)
+// Division :       a / b / c / d = (a * d) / (b * c)
+// The user should type the first fraction, an operator,and a second fraction.The program
+// should then display the result and ask whether the user wants to continue.
+
+#include <iostream>
+
+int main()
+{
+	char repeat = 'y';
+	
+	struct fraction
+	{
+		int nominator;
+		int denominator;
+	};
+
+	while (repeat == 'y')
+	{
+		fraction first;
+		fraction second;
+		fraction third;
+		
+		float result = 0.0f;
+		char i = ' ';
+		char sign = ' ';
+
+		std::cout << "Enter first fraction : for example 1/2" << std::endl;
+		std::cin >> first.nominator >> i >> first.denominator;
+		std::cout << "Enter operation '+', '-', '*', '/' ";// Выбор операции: сложение, вычитание, умножение и запрос на ввод значений
+		std::cin >> sign;
+		std::cout << "Enter second fraction : for example 2/5" << std::endl;
+		std::cin >> second.nominator >> i >> second.denominator;
+	
+		switch (sign) // калькулятор
+		{
+		case '+':
+			std::cout << "Result is: " << (first.nominator * second.denominator + first.denominator * second.nominator) << "/" << (first.denominator * second.denominator) << std::endl;
+			break;
+		case '-':
+			std::cout << "Result is: " << (first.nominator * second.denominator - first.denominator * second.nominator) << "/" << (first.denominator * second.denominator) << std::endl;
+			break;
+		case '*':
+			std::cout << "Result is: " << (first.nominator * second.nominator) << "/" << (first.denominator * second.denominator) << std::endl;
+			break;
+		case '/':
+			std::cout << "Result is: " << (first.nominator * second.denominator) << "/" << (first.denominator * second.nominator) << std::endl;
+			break;
+			default:
+				std::cout << "Warning: \"" << sign << "\" is unknown operator" << std::endl;
+		}
+
+			std::cout << "Do another(y / n) ? "; // воспрос повторить или нет
+			std::cin >> repeat;
+		}
+
+		return EXIT_SUCCESS;
+}
