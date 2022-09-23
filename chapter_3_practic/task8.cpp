c3a0aa43a8b2eb38008d65e920ca2720982d307a
Dmitriy-Romanchuk <112806589+Dmitriy-Romanchuk@users.noreply.github.com>
2022-09-23 17:58:17 +0300

    Chapter 3 practic (#3)
    * initial commit
    
    * Added solutions for exercises 1 - 12
    
    * renamed folder
    
    * renamed folder again
    
    * Fix after review

---
+++ chapter_3_practic/task8.cpp
@@ -0,0 +1,53 @@
+﻿// Write a program that repeatedly asks the user to enter two money amounts expressed in
+// old - style British currency : pounds, shillings, and pence. (See Exercises 10 and 12 in
+// Chapter 2, “C++ Programming Basics.”) The program should then add the two amounts
+// and display the answer, again in pounds, shillings, and pence.Use a do loop that asks the
+// user whether the program should be terminated.Typical interaction might be
+// Enter first amount : £5.10.6
+// Enter second amount : £3.2.6
+// Total is £8.13.0
+// Do you wish to continue (y / n) ?
+// To add the two amounts, you’ll need to carry 1 shilling when the pence value is greater
+// than 11, and carry 1 pound when there are more than 19 shillings.
+
+#include <iostream>
+
+int main()
+{
+	char repeat = 'y';
+
+	while (repeat == 'y')
+	{
+		int  pounds1 = 0, shillings1 = 0, pence1 = 0;
+		int  pounds2 = 0, shillings2 = 0, pence2 = 0;
+		int  pounds = 0, shillings = 0, pence = 0; // for results
+		char ch = ' ';
+
+		std::cout << "Enter first amount: (5.10.6)";
+		std::cin >> pounds1 >> ch >> shillings1 >> ch >> pence1;
+
+		std::cout << "Enter second amount: (3.2.6)";
+		std::cin >> pounds2 >> ch >> shillings2 >> ch >> pence2;
+
+		pence = pence1 + pence2; // подсчет пенсов и, если их больше 12, то +1 к шиллингам
+		while (pence > 11)
+		{
+			pence -= 12;
+			shillings += 1;
+		}
+
+		shillings += (shillings1 + shillings2); //подсчет шиллингов, и если их больше 20, то + 1 к фунтам
+		while (shillings > 19)
+		{
+			shillings -= 20;
+			pounds += 1;
+		}
+		pounds += (pounds1 + pounds2); // подсчет фунтов
+
+		std::cout << pounds << "." << shillings << "." << pence << std::endl;
+		std::cout << "Do another(y / n) ? ";
+		std::cin >> repeat;
+	}
+
+	return EXIT_SUCCESS;
+}
---
+++ chapter_3_practic/task9.cpp
@@ -0,0 +1,33 @@
+﻿// Suppose you give a dinner party for six guests, but your table seats only four.In how
+// many ways can four of the six guests arrange themselves at the table ? Any of the six
+// guests can sit in the first chair.Any of the remaining five can sit in the second chair.Any
+// of the remaining four can sit in the third chair, and any of the remaining three can sit in
+// the fourth chair. (The last two will have to stand.) So the number of possible arrangements of six guests in four chairs is 6 * 5 * 4 * 3, which is 360. 
+// Write a program that calculates the number of possible arrangements for any number of guestsand any number of
+// chairs. (Assume there will never be fewer guests than chairs.) Don’t let this get too complicated.A simple for loop should do it.
+
+#include <iostream>
+
+int main()
+{
+	int seats = 0;
+	int guests = 0;
+	unsigned int counter = 1;
+
+	std::cout << "Enter the number of seats at the table: ";
+	std::cin >> seats;
+	do
+	{
+		std::cout << "Enter the number of guests: (more than seats) ";
+		std::cin >> guests;
+	} while (seats > guests);
+
+	for (int i = seats; i > 0; i--, guests--)
+	{
+		counter *= guests;
+
+	}
+	std::cout << counter;
+
+	return EXIT_SUCCESS;
+}
