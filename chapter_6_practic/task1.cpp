6a9868a95d724dc656e7e678a68ee22a9d855cd8
dmitriy_romanchuk <roman4ukdimka@mail.ru>
2022-09-26 21:39:07 +0300

    Add solution for task 1 - 5

---
+++ chapter_6_practic/task1.cpp
@@ -0,0 +1,40 @@
+﻿// Create a class that imitates part of the functionality of the basic data type int.Call the
+// class Int(note different capitalization).The only data in this class is an int variable.
+// Include member functions to initialize an Int to 0, to initialize it to an int value, 
+// to display it(it looks just like an int), and to add two Int values.
+// Write a program that exercises this class by creating one uninitializedand two initialized
+// Int values, adding the two initialized valuesand placing the response in the uninitialized
+// value, and then displaying this result
+
+#include <iostream>
+
+class Int
+{
+private:
+	int value;
+public:
+	Int() : value(0)
+	{	}
+	Int(int a) : value(a)
+	{	}
+	void show_value() const
+	{
+		std::cout << value;
+	}
+	void add_value(Int b, Int c)
+	{
+		value = b.value + c.value;
+	}
+};
+
+int main()
+{
+	Int one(20);
+	Int two(5);
+	Int three;
+
+	three.add_value(one, two);
+	three.show_value();
+
+	return EXIT_SUCCESS;
+}
---
+++ chapter_6_practic/task5.cpp
@@ -0,0 +1,39 @@
+// Start with the date structure in Exercise 5 in Chapter 4 and transform it into a date
+// class.Its member data should consist of three ints : month, day, and year.It should also
+// have two member functions : getdate(), which allows the user to enter a date in
+// 12 / 31 / 02 format, and showdate(), which displays the date.
+
+#include <iostream>
+#include <iomanip>
+
+class date
+{
+private:
+	int day;
+	int month;
+	int year;
+	char ch;
+public:
+	date() : month(0), day(0), year(0), ch(' ')
+	{}
+	void getdate()
+	{
+		std::cout << "Enter date in format dd/mm/yyyy: ";
+		std::cin >> day >> ch >> month >> ch >> year;
+	}
+	void showdate() const
+	{
+		std::cout << std::setiosflags(std::ios::right) << std::setfill('0') << "You Entered: "
+			<< std::setw(2) << day << ch << std::setw(2) << month << ch << std::setw(4) << year << std::endl;
+	}
+};
+
+int main()
+{
+	date date1;
+
+	date1.getdate();
+	date1.showdate();
+
+	return EXIT_SUCCESS;
+}
