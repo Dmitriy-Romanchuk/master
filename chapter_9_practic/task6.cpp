e5149daa551b5f9a7e790cf8f361385dada2c996
Dmitriy-Romanchuk <112806589+Dmitriy-Romanchuk@users.noreply.github.com>
2022-10-30 13:54:55 +0200

    Solution for task5

---
+++ chapter_9_practic/task6.cpp
@@ -0,0 +1,5 @@
+// Start with the ARROVER3 program in Chapter 8. Keep the safearay class the same as in
+// that program, and, using inheritance, derive the capability for the user to specify both the
+// upperand lower bounds of the array in a constructor.This is similar to Exercise 9 in
+// Chapter 8, except that inheritance is used to derive a new class (you can call it safehilo)
+// instead of modifying the original class.
