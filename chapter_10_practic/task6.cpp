f4659d176f87024118d9a19f007b4d4aff878e3b
Dmitriy-Romanchuk <112806589+Dmitriy-Romanchuk@users.noreply.github.com>
2022-11-09 20:20:39 +0200

    Create task6.cpp

---
+++ chapter_10_practic/task6.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int compstr(char*, char*);
+
+int main()
+{
+	char a = 'c';
+	char b = 'b';
+
+	if (b > a)
+		std::cout << "Test";
+	if (a > b)
+		std::cout << "!!!";
+
+	return EXIT_SUCCESS;
+}
+
+int compstr(char* s1, char* s2)
+{	
+	
+
+}
